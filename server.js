2be7f3da904e203c7e9e5c9c8d17826ed151e8fb
Phil Lang <plangphoto@gmail.com>
2018-10-18 12:54:45 +0100

    Add express and express layout dependencies, improve server setup and routes

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 3000;
+const path = require('path')
+const express = require('express')
+const layout = require('express-layout')
 
-app.use(express.static('public'));
+const routes = require('./routes')
+const app = express()
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
-app.listen(port, function () {
- console.log(`Example app listening on port:` + port);
-});
+const middlewares = [
+  layout(),
+  express.static(path.join(__dirname, 'public'))
+]
+app.use(middlewares)
+
+app.use('/', routes)
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(3000, () => {
+  console.log(`App running at http://localhost:3000`)
+})
